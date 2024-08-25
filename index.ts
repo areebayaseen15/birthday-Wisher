cbe1b11d7cf4fdf240642e918adb8c91d46cdfce
Areeba Yaseen <147875287+areebayaseen15@users.noreply.github.com>
2024-08-26 03:05:01 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,60 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import figlet from "figlet";E
+    type: "input",
+    message: chalk.bgCyanBright("Enter the name whom you want to wish!"),
+  });
+
+  console.log(
+    chalk.yellowBright(
+      `               *****************Today is someone's birthday. Let's Count******************`
+    )
+  );
+
+  function startTimer(value: number) {
+    const initialTime = new Date().setSeconds(new Date().getSeconds() + value);
+    const intervalTime = new Date(initialTime);
+
+    const intervalId = setInterval(() => {
+      const currentTime = new Date();
+      const timeDiff = differenceInSeconds(intervalTime, currentTime);
+      if (timeDiff <= 0) {
+        clearInterval(intervalId);
+        console.log(chalk.green("Time's up!"));
+        displayBirthdayMessage();
+        return;
+      }
+      const min = Math.floor((timeDiff % 3600) / 60);
+      const sec = Math.floor(timeDiff % 60);
+      console.log(
+        chalk.yellow(`${min.toString().padStart(2, "0")}: ${sec.toString().padStart(2, "0")}`)
+      );
+    }, 1000);
+  }
+
+  function displayBirthdayMessage() {
+    console.log(
+      chalk.red(`
+                                                | | | |                     
+                                              (.........)                   
+                                              (>>>>>>>>>)
+                                              (_________)
+    `)
+    );
+
+    console.log(chalk.magentaBright(figlet.textSync(`Happy Birthday ${name.HBDPerson}`)));
+
+    const animation = chalkAnimation.rainbow(
+      '\n        Wish You a very Happy Birthday. May this year bring you joy, happiness, and fulfillment'
+    );
+
+    setTimeout(() => {
+      animation.stop(); // Stop the animation after a certain time
+    }, 5000);
+  }
+
+  startTimer(10);
+}
+
+birthdayWish();
